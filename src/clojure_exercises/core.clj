3711693aa657674e1a952d13e2b1afd8ddf3cc0c
Gal Hayat <gal.hayat@zesty.co>
2022-05-24 18:13:12 +0300

    optimization

--- src/clojure_exercises/core.clj
+++ src/clojure_exercises/core.clj
@@ -3,13 +3,16 @@
 
 (defn greatest-sum
   ([arr]
-   (greatest-sum arr {"start" 0 "end" (dec (count arr)) "sum" (apply + arr)} 0 (dec (count arr))))
-  ([arr greatest-obj start end]
-   (if (= (count arr) 1)
-     (apply max-key #(% "sum") [{"start" start "end" end "sum" (first arr)} greatest-obj])
-     (let [new-greatest-obj (max-key #(% "sum") {"start" start "end" end "sum" (apply + arr)} greatest-obj)]
-           (apply max-key #(% "sum") [(greatest-sum (rest arr) new-greatest-obj (inc start) end)
-                                      (greatest-sum (drop-last arr) new-greatest-obj start (dec end))])))))
+   (greatest-sum arr nil (apply + arr) 0 (dec (count arr))))
+  ([arr greatest-obj arr-sum start end]
+   (let [arr-obj {"start" start "end" end "sum" arr-sum}
+         new-greatest-obj (if (nil? greatest-obj)
+                            arr-obj
+                            (max-key #(% "sum") arr-obj greatest-obj))]
+    (if (= (count arr) 1)
+      new-greatest-obj
+      (apply max-key #(% "sum") [(greatest-sum (rest arr) new-greatest-obj (- arr-sum (first arr)) (inc start) end)
+                                 (greatest-sum (drop-last arr) new-greatest-obj (- arr-sum (last arr)) start (dec end))])))))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/clojure_exercises/core_test.clj
+++ test/clojure_exercises/core_test.clj
@@ -3,8 +3,7 @@
             [clojure-exercises.core :refer :all]))
 
 (deftest greatest-sum-test []
-    (is (= (greatest-sum [1 2 3 4 5]) {"start" 0 "end" 4 "sum" 15})))
+    (is (= (greatest-sum [1 2 3 4 5]) {"start" 0 "end" 4 "sum" 15}))
+    (is (= (greatest-sum [-1 2 3 -4 5]) {"start" 1 "end" 4 "sum" 6}))
+    (is (= (greatest-sum [-1 2 3 -4 5 4 -3 7 9 -4]) {"start" 1 "end" 8 "sum" 23})))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
